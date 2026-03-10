4c5c5fc62076c42f1df114a48a3233d5b0a53f13
danielekaufman <kaufman.dan@gmail.com>
2026-03-10 11:10:28 -0700

    Fix holdtotalk cask URL to use correct repo name
    The cask URL and homepage pointed to `jxucoder/holdtotalk` which doesn't
    have releases, causing a 404 on install. The actual repo is
    `jxucoder/hold-to-talk` (with hyphens).

--- Casks/holdtotalk.rb
+++ Casks/holdtotalk.rb
@@ -2,10 +2,10 @@ cask "holdtotalk" do
   version "1.0.18"
   sha256 "daa07b4f595b053b3fb846bebfe29ac4dd0f5dd7fff19ca70b2346cde4fe67de"
 
-  url "https://github.com/jxucoder/holdtotalk/releases/download/v#{version}/HoldToTalk-v#{version}.zip"
+  url "https://github.com/jxucoder/hold-to-talk/releases/download/v#{version}/HoldToTalk-v#{version}.zip"
   name "Hold to Talk"
   desc "Free, open-source voice dictation for macOS"
-  homepage "https://github.com/jxucoder/holdtotalk"
+  homepage "https://github.com/jxucoder/hold-to-talk"
 
   depends_on macos: ">= :sonoma"
 
