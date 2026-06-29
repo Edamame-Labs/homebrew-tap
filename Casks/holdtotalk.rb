71a35feb3cf5b8ab8c78cad471593cfba2b2d8b1
J Xu <jxucoder@gmail.com>
2026-06-28 20:52:23 -0700

    Update Hold to Talk cask URLs

--- Casks/holdtotalk.rb
+++ Casks/holdtotalk.rb
@@ -2,10 +2,10 @@ cask "holdtotalk" do
   version "2.1.5"
   sha256 "b24ba394fc61b29360de45ac678823e4f2a5ee6870499ec005b215ce46d38634"
 
-  url "https://github.com/jxucoder/hold-to-talk/releases/download/v#{version}/HoldToTalk-v#{version}.zip"
+  url "https://github.com/Edamame-Labs/hold-to-talk/releases/download/v#{version}/HoldToTalk-v#{version}.zip"
   name "Hold to Talk"
   desc "Free, open-source voice dictation for macOS"
-  homepage "https://github.com/jxucoder/hold-to-talk"
+  homepage "https://github.com/Edamame-Labs/hold-to-talk"
 
   depends_on macos: ">= :sequoia"
 
